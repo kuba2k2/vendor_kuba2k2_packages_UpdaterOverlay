1d25e7c8360cd6f86a7ad6a922771764c69248fc
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-08-05 22:25:05 +0200

    Set LOCAL_PRODUCT_MODULE

--- Android.mk
+++ Android.mk
@@ -5,5 +5,7 @@ LOCAL_PACKAGE_NAME := UpdaterOverlay
 LOCAL_RRO_THEME := UpdaterOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 include $(BUILD_RRO_PACKAGE)
